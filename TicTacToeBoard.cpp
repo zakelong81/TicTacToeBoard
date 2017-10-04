cffba545f9e0202f870e368f915c4296710e6930
Long Nguyen <zakelong81@github.com>
2017-10-04 14:19:11 -0700

    FIX BUG TRAVIS

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -61,17 +61,16 @@ Piece TicTacToeBoard::getPiece(int row, int column){return board[row][column];}
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  for(int i = 1; i < BOARDSIZE; i++)
+  for(int i=0;i<BOARDSIZE; i++)
   {
    if(board[i][0]==board[i][1] && board[i][0]==board[i][2])
-        return board[i][0];
+     return board[i][0];
    else if(board[0][i]==board[1][i] && board[1][i] == board[2][i])
-        return board[1][i];
+     return board[1][i];
   }
-
   if(board[0][0] == board[1][1] && board[1][1] == board[2][2])
-      return board[0][0];
+    return board[0][0];
   if(board[0][2] == board[1][1] && board[1][1] == board[2][0])
-      return board[1][1];
-   return Invalid;
+    return board[1][1];
+  return Invalid;
 }
